5baf03c772f0a9b0bf45114ba0c7dd1b95dc185d
AMIRIOX <wumingyun2120@outlook.com>
2025-03-21 17:36:32 +0800

    last one!

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,8 +1,7 @@
 /*
-	graph
-	This problem requires you to implement a basic graph functio
+    graph
+    This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -29,7 +28,13 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        self.add_node(edge.0);
+        let mut entry = self.adjacency_table_mutable().get_mut(edge.0).unwrap();
+        entry.push((edge.1.to_string(), edge.2));
+
+        self.add_node(edge.1);
+        let mut entry = self.adjacency_table_mutable().get_mut(edge.1).unwrap();
+        entry.push((edge.0.to_string(), edge.2));
     }
 }
 pub trait Graph {
@@ -37,11 +42,19 @@ pub trait Graph {
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        let table = self.adjacency_table_mutable();
+        match table.get_mut(node) {
+            None => {
+                table.insert(node.to_string(), Vec::new());
+                true
+            }
+            _ => false,
+        }
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        self.add_node(edge.0);
+        let mut entry = self.adjacency_table_mutable().get_mut(edge.0).unwrap();
+        entry.push((edge.1.to_string(), edge.2));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
@@ -66,6 +79,7 @@ mod test_undirected_graph {
     #[test]
     fn test_add_edge() {
         let mut graph = UndirectedGraph::new();
+
         graph.add_edge(("a", "b", 5));
         graph.add_edge(("b", "c", 10));
         graph.add_edge(("c", "a", 7));
